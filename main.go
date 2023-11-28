dadb258adb8eec6d4c8cb0baf9c4d24b9e6cad23
H0llyW00dzZ <priv8@btz.pm>
2023-11-29 02:24:40 +0700

    Feat & Fix Golang Formatting in CSV (#3)
    - [+] feat(main.go): add support for creating separate CSV files for sessions and messages
    - [+] fix(main.go): fix formatting issue in convertSessionsToCSV function

--- main.go
+++ main.go
@@ -68,26 +68,27 @@ func readJSONFromFile(filePath string) (ChatNextWebStore, error) {
 
 // Function to convert sessions to CSV format
 func convertSessionsToCSV(sessions []Session, formatOption int) (string, error) {
-	var csvData [][]string
+	var (
+		csvData [][]string
+		headers []string
+	)
 
 	switch formatOption {
 	case 1: // Inline Formatting
+		headers = []string{"id", "topic", "memoryPrompt", "messages"}
 		for _, session := range sessions {
 			var messageContents []string
 			for _, message := range session.Messages {
-				messageContents = append(messageContents, fmt.Sprintf("[%s, %s] %s", message.Role, message.Date, message.Content))
+				messageContents = append(messageContents, fmt.Sprintf("[%s, %s] \"%s\"", message.Role, message.Date, message.Content))
 			}
 			sessionData := []string{
-				session.ID, session.Topic, session.MemoryPrompt,
-				fmt.Sprint(session.Stat.TokenCount), fmt.Sprint(session.Stat.WordCount), fmt.Sprint(session.Stat.CharCount),
-				strconv.FormatInt(session.LastUpdate, 10), strings.Join(messageContents, "; "),
-				session.Mask.ID, session.Mask.Avatar, session.Mask.Name, session.Mask.Lang, strconv.FormatInt(session.Mask.CreatedAt, 10),
-				strings.Join(messageContents, "; "),
+				session.ID, session.Topic, session.MemoryPrompt, strings.Join(messageContents, "; "),
 			}
 			csvData = append(csvData, sessionData)
 		}
 
 	case 2: // One Message Per Line
+		headers = []string{"session_id", "message_id", "date", "role", "content", "memoryPrompt"}
 		for _, session := range sessions {
 			for _, message := range session.Messages {
 				sessionData := []string{
@@ -97,21 +98,15 @@ func convertSessionsToCSV(sessions []Session, formatOption int) (string, error) 
 			}
 		}
 
-	case 3: // Separate Files for Sessions and Messages (sessions part)
-		// This will be handled differently in the main logic (TODO)
-
 	case 4: // JSON String in CSV
+		headers = []string{"id", "topic", "memoryPrompt", "messages"}
 		for _, session := range sessions {
 			messagesJSON, err := json.Marshal(session.Messages)
 			if err != nil {
 				return "", err
 			}
 			sessionData := []string{
-				session.ID, session.Topic, session.MemoryPrompt,
-				fmt.Sprint(session.Stat.TokenCount), fmt.Sprint(session.Stat.WordCount), fmt.Sprint(session.Stat.CharCount),
-				strconv.FormatInt(session.LastUpdate, 10), string(messagesJSON),
-				session.Mask.ID, session.Mask.Avatar, session.Mask.Name, session.Mask.Lang, strconv.FormatInt(session.Mask.CreatedAt, 10),
-				string(messagesJSON),
+				session.ID, session.Topic, session.MemoryPrompt, string(messagesJSON),
 			}
 			csvData = append(csvData, sessionData)
 		}
@@ -123,13 +118,68 @@ func convertSessionsToCSV(sessions []Session, formatOption int) (string, error) 
 	// Convert the slice of slices to CSV string
 	csvString := &strings.Builder{}
 	csvWriter := csv.NewWriter(csvString)
-	err := csvWriter.WriteAll(csvData)
-	if err != nil {
+	if err := csvWriter.Write(headers); err != nil {
+		return "", err
+	}
+	if err := csvWriter.WriteAll(csvData); err != nil {
 		return "", err
 	}
 	csvWriter.Flush()
 
 	return csvString.String(), nil
+}
+
+// Function to create separate CSV files for sessions and messages
+func createSeparateCSVFiles(sessions []Session, sessionsFileName string, messagesFileName string) error {
+	// Create CSV file for sessions
+	sessionsFile, err := os.Create(sessionsFileName)
+	if err != nil {
+		return err
+	}
+	defer sessionsFile.Close()
+
+	sessionsWriter := csv.NewWriter(sessionsFile)
+	defer sessionsWriter.Flush()
+
+	sessionsHeaders := []string{"id", "topic", "memoryPrompt"} // Add other headers as needed
+	if err := sessionsWriter.Write(sessionsHeaders); err != nil {
+		return err
+	}
+	for _, session := range sessions {
+		sessionData := []string{
+			session.ID, session.Topic, session.MemoryPrompt,
+		}
+		if err := sessionsWriter.Write(sessionData); err != nil {
+			return err
+		}
+	}
+
+	// Create CSV file for messages
+	messagesFile, err := os.Create(messagesFileName)
+	if err != nil {
+		return err
+	}
+	defer messagesFile.Close()
+
+	messagesWriter := csv.NewWriter(messagesFile)
+	defer messagesWriter.Flush()
+
+	messagesHeaders := []string{"session_id", "message_id", "date", "role", "content", "memoryPrompt"}
+	if err := messagesWriter.Write(messagesHeaders); err != nil {
+		return err
+	}
+	for _, session := range sessions {
+		for _, message := range session.Messages {
+			messageData := []string{
+				session.ID, message.ID, message.Date, message.Role, message.Content, session.MemoryPrompt,
+			}
+			if err := messagesWriter.Write(messageData); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
 
 // Function to extract JSON sessions to Hugging Face dataset
@@ -182,36 +232,56 @@ func main() {
 			return
 		}
 
-		// Convert the sessions to CSV according to the chosen format
-		csvOutput, err := convertSessionsToCSV(store.ChatNextWebStore.Sessions, formatOption)
-		if err != nil {
-			fmt.Printf("Failed to convert sessions to CSV: %s\n", err)
+		if formatOption == 3 {
+			fmt.Println("Enter the name of the sessions CSV file to save:")
+			sessionsFileName, _ := reader.ReadString('\n')
+			sessionsFileName = strings.TrimSpace(sessionsFileName)
+
+			fmt.Println("Enter the name of the messages CSV file to save:")
+			messagesFileName, _ := reader.ReadString('\n')
+			messagesFileName = strings.TrimSpace(messagesFileName)
+
+			err := createSeparateCSVFiles(store.ChatNextWebStore.Sessions, sessionsFileName, messagesFileName)
+			if err != nil {
+				fmt.Printf("Failed to create separate CSV files: %s\n", err)
+				return
+			}
+
+			fmt.Printf("Sessions data saved to %s\n", sessionsFileName)
+			fmt.Printf("Messages data saved to %s\n", messagesFileName)
 			return
-		}
-
-		// Output the CSV content
-		fmt.Println(csvOutput)
-
-		// Optionally, you can save this output to a file
-		fmt.Println("Do you want to save the output to a file? (yes/no)")
-		saveOutput, _ := reader.ReadString('\n')
-		saveOutput = strings.TrimSpace(saveOutput)
-		if saveOutput == "yes" {
-			fmt.Println("Enter the name of the CSV file to save:")
-			csvFileName, _ := reader.ReadString('\n')
-			csvFileName = strings.TrimSpace(csvFileName)
-			file, err := os.Create(csvFileName)
+		} else {
+			// Convert the sessions to CSV according to the chosen format
+			csvOutput, err := convertSessionsToCSV(store.ChatNextWebStore.Sessions, formatOption)
 			if err != nil {
-				fmt.Printf("Failed to create the CSV file: %s\n", err)
+				fmt.Printf("Failed to convert sessions to CSV: %s\n", err)
 				return
 			}
-			defer file.Close()
-			_, err = file.WriteString(csvOutput)
-			if err != nil {
-				fmt.Printf("Failed to write to the CSV file: %s\n", err)
-				return
+
+			// Output the CSV content
+			fmt.Println(csvOutput)
+
+			// Optionally, you can save this output to a file
+			fmt.Println("Do you want to save the output to a file? (yes/no)")
+			saveOutput, _ := reader.ReadString('\n')
+			saveOutput = strings.TrimSpace(saveOutput)
+			if saveOutput == "yes" {
+				fmt.Println("Enter the name of the CSV file to save:")
+				csvFileName, _ := reader.ReadString('\n')
+				csvFileName = strings.TrimSpace(csvFileName)
+				file, err := os.Create(csvFileName)
+				if err != nil {
+					fmt.Printf("Failed to create the CSV file: %s\n", err)
+					return
+				}
+				defer file.Close()
+				_, err = file.WriteString(csvOutput)
+				if err != nil {
+					fmt.Printf("Failed to write to the CSV file: %s\n", err)
+					return
+				}
+				fmt.Printf("CSV output saved to %s\n", csvFileName)
 			}
-			fmt.Printf("CSV output saved to %s\n", csvFileName)
 		}
 
 	case "2":
