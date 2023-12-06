d9f4d6e4be3eb14dbfeff3f857c50301a42df1e6
H0llyW00dzZ <priv8@btz.pm>
2023-12-06 13:16:58 +0700

    Feat & Fix [Golang] Main Command
    - [+] feat(main.go): add support for repairing JSON data
    - [+] fix(main.go): update jsonFilePath to the new repaired file path

--- main.go
+++ main.go
@@ -21,28 +21,37 @@ func main() {
 	// Get the JSON file path from the user
 	jsonFilePath := promptForInput(reader, "Enter the path to the JSON file: ")
 
-	// Read the old JSON content from the file
-	oldJSONBytes, err := os.ReadFile(jsonFilePath)
-	if err != nil {
-		fmt.Printf("Error reading the JSON file: %s\n", err)
-		os.Exit(1)
+	// Prompt the user to decide whether to repair data
+	repairData := promptForInput(reader, "Do you want to repair data? (yes/no): ")
+	if strings.ToLower(repairData) == "yes" {
+		// Read the old JSON content from the file
+		oldJSONBytes, err := os.ReadFile(jsonFilePath)
+		if err != nil {
+			fmt.Printf("Error reading the JSON file: %s\n", err)
+			os.Exit(1)
+		}
+
+		// Repair the old JSON data using the repairdata package
+		newJSONBytes, err := repairdata.RepairSessionData(oldJSONBytes)
+		if err != nil {
+			fmt.Printf("Error repairing the JSON data: %s\n", err)
+			os.Exit(1)
+		}
+
+		// Write the repaired JSON data to a new file
+		newFilePath := strings.TrimSuffix(jsonFilePath, ".json") + "_repaired.json"
+		err = os.WriteFile(newFilePath, newJSONBytes, 0644)
+		if err != nil {
+			fmt.Printf("Error writing the new JSON data to file: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Repaired JSON data has been saved to: %s\n", newFilePath)
+
+		// Update the jsonFilePath to the new repaired file path
+		jsonFilePath = newFilePath
 	}
 
-	// Repair the old JSON data using the repairdata package
-	newJSONBytes, err := repairdata.RepairSessionData(oldJSONBytes)
-	if err != nil {
-		fmt.Printf("Error repairing the JSON data: %s\n", err)
-		os.Exit(1)
-	}
-
-	// Write the repaired JSON data to a new file
-	newFilePath := strings.TrimSuffix(jsonFilePath, ".json") + "_repaired.json"
-	err = os.WriteFile(newFilePath, newJSONBytes, 0644)
-	if err != nil {
-		fmt.Printf("Error writing the new JSON data to file: %s\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Repaired JSON data has been saved to: %s\n", newFilePath)
+	// Continue processing with the jsonFilePath (which is either the original or the repaired file)
 	// Read the JSON content using the exporter package
 	store, err := exporter.ReadJSONFromFile(jsonFilePath)
 	if err != nil {
