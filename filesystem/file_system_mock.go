6b9ba8ebd67bee7caf765ad69414160b5ab79e12
H0llyW00dzZ <priv8@btz.pm>
2023-12-09 11:29:37 +0700

    Feat [Golang] [Package] File System Mock
    - [+] feat(filesystem): add mock implementation of the FileSystem interface for testing purposes
    - [+] feat(filesystem): add mock implementation of the exporter.Exporter interface for testing purposes
    - [+] fix(filesystem): update package comment to include purpose of the mock implementation
    - [+] fix(filesystem): update MockFileSystem to use map instead of slice to track created files
    - [+] fix(filesystem): update Create method to create a new buffer in the FilesCreated map
    - [+] fix(filesystem): update Stat method to return mock file information if file exists in the mock file system
    - [+] fix(filesystem): update ReadFile method to read content of a file from the FilesCreated map
    - [+] fix(filesystem): update WriteFile method to write data to a file in the FilesCreated map
    - [+] feat(filesystem): add dummy implementation of mockFileInfo for testing purposes

--- filesystem/file_system_mock.go
+++ filesystem/file_system_mock.go
@@ -1,43 +1,90 @@
-// Package filesystem provides a mock implementation of the FileSystem interface for testing.
+// Package filesystem provides a mock implementation of the FileSystem interface for testing purposes.
+// It allows for tracking file operations and simulating file system interactions without actual disk I/O (Magic Golang 🎩 🪄).
+//
+// Copyright 2023 H0llyW00dzZ
 package filesystem
 
 import (
+	"bytes"
+	"context"
 	"io/fs"
 	"os"
+	"time"
+
+	"github.com/H0llyW00dzZ/ChatGPT-Next-Web-Session-Exporter/exporter"
 )
 
+// Ensure MockFileSystem adheres to the FileSystem interface.
+var _ FileSystem = (*MockFileSystem)(nil)
+
 // MockFileSystem is a mock implementation of the FileSystem interface that can be used in tests.
-// It tracks the file operations performed, such as file creation, without interacting with the actual file system.
+// It uses a map to store file names and associated data, allowing for the simulation of file creation,
+// reading, and writing without actual file system interaction.
 type MockFileSystem struct {
-	FilesCreated []string // FilesCreated keeps track of the names of the files that have been "created".
+	FilesCreated map[string]*bytes.Buffer // FilesCreated maps file names to buffers holding file contents.
 }
 
-// Stat is part of the FileSystem interface. It is meant to return the FileInfo for the given file name.
-// In the MockFileSystem, this function is unimplemented and will panic if called, indicating the function
-// should be stubbed or the test should be structured to avoid its call.
-func (*MockFileSystem) Stat(name string) (fs.FileInfo, error) {
-	panic("unimplemented")
+// MockExporter is a mock implementation of the exporter.Exporter interface for testing purposes.
+// It allows for the simulation of session conversion to CSV format and can be set to return errors for testing error handling.
+type MockExporter struct {
+	ErrToReturn error // ErrToReturn is the error that ConvertSessionsToCSV will return when called.
 }
 
-// Create simulates the creation of a file by adding the provided file name to the FilesCreated slice.
-// It returns a dummy *os.File object that can be used in place of a real file in tests.
-// This allows tests to verify that a file creation operation was initiated without actually creating a file on disk.
+// ConvertSessionsToCSV simulates the conversion of sessions to CSV format.
+// It returns an error specified by ErrToReturn, allowing for error handling tests.
+func (m *MockExporter) ConvertSessionsToCSV(ctx context.Context, sessions []exporter.Session, formatOption int, csvFileName string) error {
+	return m.ErrToReturn
+}
+
+// Stat returns the FileInfo for the given file name if it exists in the mock file system.
+// If the file does not exist, it returns an error to simulate the os.Stat behavior.
+func (m *MockFileSystem) Stat(name string) (fs.FileInfo, error) {
+	if _, ok := m.FilesCreated[name]; ok {
+		// Return mock file information. In real tests, you might want to return a more detailed mock.
+		return mockFileInfo{name: name}, nil
+	}
+	return nil, fs.ErrNotExist
+}
+
+// Create simulates the creation of a file by creating a new buffer in the FilesCreated map.
+// It returns a dummy *os.File object and nil error to mimic the os.Create function's behavior in tests.
 func (m *MockFileSystem) Create(name string) (*os.File, error) {
-	m.FilesCreated = append(m.FilesCreated, name)
-	// Here we return a dummy file using os.NewFile. The file descriptor 0 signifies that this is not associated
-	// with an open file. The name is used for identification in tests.
+	if m.FilesCreated == nil {
+		m.FilesCreated = make(map[string]*bytes.Buffer)
+	}
+	m.FilesCreated[name] = new(bytes.Buffer)
+	// Return a dummy file object with a fake file descriptor.
 	return os.NewFile(0, name), nil
 }
 
-// WasCalledWith checks whether the Create method was called with a specific file name.
-// It returns true if the name is in the FilesCreated slice, indicating that a create operation was requested
-// for the file; otherwise, it returns false. This is useful for assertions in tests to ensure that
-// the correct file operations were performed.
-func (m *MockFileSystem) WasCalledWith(name string) bool {
-	for _, fileCreated := range m.FilesCreated {
-		if fileCreated == name {
-			return true
-		}
+// ReadFile simulates reading the content of a file from the FilesCreated map.
+// If the file exists, it returns the content as a byte slice; otherwise, it returns an error.
+func (m *MockFileSystem) ReadFile(name string) ([]byte, error) {
+	if content, ok := m.FilesCreated[name]; ok {
+		return content.Bytes(), nil
 	}
-	return false
+	return nil, fs.ErrNotExist
 }
+
+// WriteFile simulates writing data to a file in the FilesCreated map.
+// It creates a new buffer with the provided data, simulating a successful write operation.
+func (m *MockFileSystem) WriteFile(name string, data []byte, perm fs.FileMode) error {
+	if m.FilesCreated == nil {
+		m.FilesCreated = make(map[string]*bytes.Buffer)
+	}
+	m.FilesCreated[name] = bytes.NewBuffer(data)
+	return nil
+}
+
+// mockFileInfo is a dummy implementation of fs.FileInfo used for testing.
+// It provides basic implementations of the fs.FileInfo interface methods.
+type mockFileInfo struct {
+	name string // name is the file name.
+}
+
+func (m mockFileInfo) Name() string       { return m.name }
+func (m mockFileInfo) Size() int64        { return 0 }           // Dummy value for size.
+func (m mockFileInfo) Mode() fs.FileMode  { return 0 }           // Dummy value for file mode.
+func (m mockFileInfo) ModTime() time.Time { return time.Time{} } // Dummy value for modification time.
+func (m mockFileInfo) IsDir() bool        { return false }       // Dummy value, always false.
+func (m mockFileInfo) Sys() interface{}   { return nil }         // No system-specific information.
