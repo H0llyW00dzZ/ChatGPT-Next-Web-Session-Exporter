c73d5d72704554245e87185298c047295066aae6
H0llyW00dzZ <priv8@btz.pm>
2023-12-11 22:14:26 +0700

    Docs [Golang] [Package] Banner cli
    - [+] chore(banner.go): update package comment and file path comment
    - [+] feat(banner.go): add comments to PrintBinaryBanner function
    - [+] feat(banner.go): add comments to PrintAnimatedBanner function

--- bannercli/banner.go
+++ bannercli/banner.go
@@ -1,4 +1,7 @@
-// banner/banner.go
+// Package banner provides functionality to print different styles of banners
+// to the terminal. These styles include binary representation and simple
+// animation effects to enhance the visual presentation of CLI applications.
+// # banner/banner.go
 // Copyright (c) 2023 H0llyW00dzZ
 package banner
 
@@ -9,6 +12,8 @@ import (
 )
 
 // PrintBinaryBanner prints a binary representation of a banner.
+// Each character of the message is converted into its binary form.
+// Spaces between words are widened to enhance readability.
 func PrintBinaryBanner(message string) {
 	banner := strings.ReplaceAll(message, " ", "   ")
 	for _, char := range banner {
@@ -17,7 +22,10 @@ func PrintBinaryBanner(message string) {
 	fmt.Println()
 }
 
-// PrintAnimatedBanner prints a simple animated banner by scrolling the message.
+// PrintAnimatedBanner prints a simple animated banner by scrolling the message
+// horizontally across the terminal. The animation repeats the number of times
+// specified by the `repeat` parameter with a delay between each frame as
+// specified by the `delay` parameter.
 func PrintAnimatedBanner(message string, repeat int, delay time.Duration) {
 	for r := 0; r < repeat; r++ {
 		for i := 0; i < len(message); i++ {
