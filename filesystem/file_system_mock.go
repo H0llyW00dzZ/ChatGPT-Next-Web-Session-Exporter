a6bc3fb19546c02cff5ab6e5b1b5323fa990b6ee
H0llyW00dzZ <priv8@btz.pm>
2023-12-11 17:36:09 +0700

    Chore [Docs] [Golang] [Package] Filesystem (#29)
    * Chore [Docs] [Golang] [Package] Filesystem
    
    - [+] chore(file_system_mock.go): reformat package comment for clarity and consistency
    
    * Chore [Docs] [Golang] [Package] Filesystem Additional comments
    
    - [+] chore(file_system_mock.go): add additional comments explaining the purpose and usage of the file_system_mock package
    
    * Chore [Docs] [Golang] [Package] Filesystem Additional comments
    
    - [+] chore(file_system_mock.go): update additional comments explaining the purpose and usage of the file_system_mock package

--- filesystem/file_system_mock.go
+++ filesystem/file_system_mock.go
@@ -1,5 +1,9 @@
-// Package filesystem provides a mock implementation of the FileSystem interface for testing purposes.
-// It allows for tracking file operations and simulating file system interactions without actual disk I/O (Magic Golang 🎩 🪄).
+// Below, the package filesystem (@file_system_mock.go) furnishes a mock implementation of the FileSystem
+// interface intended for testing. It enables the monitoring of file operations and
+// the emulation of file system interactions without real disk I/O, showcasing the
+// "magic" of Go programming language (🎩🪄).
+//
+// Additionally, it can be utilized for tracking file activities during tests or without tests (as Expert).
 //
 // Copyright (c) 2023 H0llyW00dzZ
 package filesystem
